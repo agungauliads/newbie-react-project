ab593c97701db05894af63d5b41a328c830fda6a
agungauliads <agungauliads@gmail.com>
2023-11-20 14:11:13 +0700

    simple web done

--- assignment-reactjs/src/App.jsx
+++ assignment-reactjs/src/App.jsx
@@ -1,11 +1,18 @@
 import NavBar from "./components/NavBar";
-
+import Hero from "./components/Hero";
+import HeadlineCards from "./components/HeadlineCards";
+import Food from "./components/Food";
+import Category from "./components/Category";
 
 function App() {
 
   return (
     <>
     <NavBar />
+    <Hero />
+    <HeadlineCards />
+    <Food />
+    <Category />
     </>
   );
 }
